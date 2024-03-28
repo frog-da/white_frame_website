c361f6085fe61a17988776bd12fa854637d9cc8e
frog-da <84839431+frog-da@users.noreply.github.com>
2024-03-28 17:53:20 +0300

    Fonts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
 import './home.scss';
 
 export default function Home() {
   return (
-    <div className='home_page'>
+    <body className='home_page'>
         <header>
             <nav className='nav'>
-                <ul className='menu-nav1'>
-                <li className={`menu-nav__item  'active'`}>
+                <ul className='menu-nav'>
+                <li className='menu-nav__item  active'>
                     <a href="/home">
                     Главная
                     </a>
                 </li>
-                <li className={`menu-nav__item`}>
+                <li className='menu-nav__item'>
                     <a href="/cases">
                     Кейсы
                     </a>
                 </li>
-                <li className={`menu-nav__item`}>
+                <li className='menu-nav__item'>
                     <a href="/services">
                     Услуги
                     </a>
                 </li>
                 </ul>
-                <ul className='menu-nav2'>
-                <li className={`menu-nav__item`}>
+                <ul className='menu-nav'>
+                <li className='menu-nav__item'>
                     <a href="/contacts">
                     Контакты
                     </a>
                 </li>
-                <li className={`menu-nav__ite`}>
+                <li className='menu-nav__item'>
                     <a href="/vacancies">
                     Вакансии
                     </a>
                 </li>
-                <li className={`menu-nav__item`}>
+                <li className='menu-nav__item'>
                     <a href="/news">
                     Новости
                     </a>
@@ -44,9 +42,36 @@ export default function Home() {
                 </ul>
             </nav>
         </header>
-      <Main />
-      <Footer />
-    </div>
+        <main>
+            <section>
+                <h1>Цифровые решения для бизнеса и государства</h1>
+                <ul className='body2'>
+                    <li>Проектируем</li>
+                    <li>Разрабатываем</li>
+                    <li>Внедряем</li>
+                    <li>Сопровождаем</li>
+                </ul>
+            </section>
+            <section>
+                <h2>MOBILE APPS</h2>
+                <p>Проектируем, разрабатываем, внедряем</p>
+            </section>
+            <section>
+                <h2>BLOCKCHAIN DEV</h2>
+                <p>Сопровождаем</p>
+            </section>
+            <footer>
+                <div>
+                    <h3>Свяжитесь с нами</h3>
+                    <p>+7 843 123 45 67</p>
+                    <p>info@whiteframe.ru</p>
+                </div>
+                <div>
+                    <p>© 2021 – White Frame, Все права защищены.</p>
+                </div>
+            </footer>
+        </main>
+    </body>
   );
 }
 
