3303f46b2e93a3410ac691964553d6f4136667b0
frog-da <84839431+frog-da@users.noreply.github.com>
2024-04-07 23:36:53 +0300

    Header animation

---
+++ src/components/header.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import '../pages/home.scss';
+
+export default function Header() {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const [startPosition, setStartPosition] = useState(true);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentScrollPos = window.scrollY;
+        setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 100);
+        setStartPosition((currentScrollPos < 100 && prevScrollPos <= currentScrollPos) || currentScrollPos == 0);
+        setPrevScrollPos(currentScrollPos);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [prevScrollPos, visible]);
+
+  return (
+    <header className={visible ? `header ${startPosition ? 'header--start' : 'header--visible'}` : 'header header--hidden'}>
+
+      <embed className='logo' src="/vectors/logo.svg" width="220" height="48"/>
+      <nav className='nav'>
+            <ul className='menu-nav'>
+                <li className='menu-nav__item  active'>
+                    <a href="/home">
+                        Главная
+                    </a>
+                </li>
+                <li className='menu-nav__item'>
+                    <a href="/cases">
+                        Кейсы
+                    </a>
+                </li>
+                <li className='menu-nav__item'>
+                    <a href="/services">
+                        Услуги
+                    </a>
+                </li>
+            </ul>
+            <ul className='menu-nav'>
+                <li className='menu-nav__item'>
+                    <a href="/contacts">
+                        Контакты
+                    </a>
+                </li>
+                <li className='menu-nav__item'>
+                    <a href="/vacancies">
+                        Вакансии
+                    </a>
+                </li>
+                <li className='menu-nav__item'>
+                    <a href="/news">
+                        Новости
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,69 +1,77 @@
 import React from 'react';
 import './home.scss';
+import Header from '../components/header';
 
 export default function Home() {
   return (
     <div className='home_page'>
-
-        <header className='header'>
-            <embed className='logo' src="/vectors/logo.svg" width="220" height="48"/>
-        
-
-            <nav className='nav'>
-                <ul className='menu-nav'>
-                    <li className='menu-nav__item  active'>
-                        <a href="/home">
-                            Главная
-                        </a>
-                    </li>
-                    <li className='menu-nav__item'>
-                        <a href="/cases">
-                            Кейсы
-                        </a>
-                    </li>
-                    <li className='menu-nav__item'>
-                        <a href="/services">
-                            Услуги
-                        </a>
-                    </li>
-                </ul>
-                <ul className='menu-nav'>
-                    <li className='menu-nav__item'>
-                        <a href="/contacts">
-                            Контакты
-                        </a>
-                    </li>
-                    <li className='menu-nav__item'>
-                        <a href="/vacancies">
-                            Вакансии
-                        </a>
-                    </li>
-                    <li className='menu-nav__item'>
-                        <a href="/news">
-                            Новости
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Header />
         <main className='main'>
             <section className='info'>
                 <h1 className='info__h1'>Цифровые решения для бизнеса и государства</h1>
                 <ul className='info__body'>
                     <li>проектируем</li>
+                    <li className='arrow'><embed className='logo' src="/vectors/arrow.svg" width="40" height="40"/></li>
                     <li>разрабатываем</li>
+                    <li className='arrow'><embed className='logo' src="/vectors/arrow.svg" width="40" height="40"/></li>
                     <li>внедряем</li>
+                    <li className='arrow'><embed className='logo' src="/vectors/arrow.svg" width="40" height="40"/></li>
                     <li>сопровождаем</li>
                 </ul>
             </section>
-            <section>
-                <h2>MOBILE APPS</h2>
-                <p>Проектируем, разрабатываем, внедряем</p>
+            <section className='tiles'>
+                <div className='column1'>
+                    <div className='column__item'>
+                        <p className='column__item__p'>Заказная разработка программного обеспечения</p>
+                    </div>
+                    <div className='column__item'>
+                        <p className='column__item__p'>Цифровая трансформация бизнеса и государства</p>
+                    </div>
+                </div>
+                <div className='column2'>
+                    <div className='column__item'>
+                        <p className='column__item__p'>Автоматизация бизнес-процессов</p>
+                    </div>
+                    <div className='column__item'>
+                        <p className='column__item__p'>Техническая поддержка информационных систем</p>
+                    </div>
+                </div>
             </section>
-            <section>
-                <h2>BLOCKCHAIN DEV</h2>
-                <p>Сопровождаем</p>
+
+            <section className='options'>
+                <div className='options__item'>
+                    <h2>вэб</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2>приложения</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2>blockchain</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2>игры</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2>дизайн</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2>интернет маркетинг</h2>
+                    <p>Разработка и поддержка порталов, сайтов, web-приложений, и много много много всего умеем.</p>
+                </div>
+                <div className='options__item'>
+                    <h2 className='options__item__h2'>консультируем :)</h2>
+                    <div className='arrow'><embed className='logo' src="/vectors/arrow.svg" width="40" height="40"/></div>
+                </div>
+                <div className='options__item'>
+                    <button className='button' type='button'>Свяжитесь с нами</button>
+                </div>
             </section>
+           
             <footer>
                 <div>
                     <h3>Свяжитесь с нами</h3>
